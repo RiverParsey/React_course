ef8e2d6e4b4be90923f5103f540041406014fb67
ricsi <rihardptrk18@gmail.com>
2025-05-11 14:15:57 +0200

    lesson-4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,52 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+// const Field = () => { // компонент Field с использованием функции
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+
+//   return <input
+//           placeholder={holder} 
+//           type='text' 
+//           style={styledField}/>
+// }
+
+class Field extends Component { // тот же компонент Field но с использованием класса а не функции
+  render() { // метод render обязателен при использовании класса
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px'
+    };
+
+    return <input
+            placeholder={holder} 
+            type='text' 
+            style={styledField}/>
+  }
+}
+ 
+function Btn() {
+  const text = 'Log in';
+  const logged = true;
+
+  return <button>{logged ? 'Enter' : text}</button> // обычные конструкции if else не работают внутри элементов
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header}; // если захочется использовать отдельный компонент вне файла
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,40 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const text = 'Hello World!';
-
-// при создании многострочного элемента всегда использовать круглые скобки
-// всегда использовать элемент обертку
-// не забывать про разные стили написания некоторых атрибутов элементов, также все они пишутся в формате cammelCase
-// всегда закрывать самозакрывающиеся теги в конце
-
-// можно вставлять все что угодно внутри фигурных скобок кроме обьектов
-
-const elem = (
-  <div>
-    <h2 className='text'>Text : {text}</h2>
-    <input type="text"/>
-    <label htmlFor=""></label>
-    <button tabIndex={0}>click me</button>
-  </div>
-);
+import { Header } from './App'; // готово к использованию
 
 
-
-// const elem = <h2>Hello World!</h2>; // создание элемента с использованием JSX
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!'); // создание элемента с использованием React
-
-// const element = {
-//   type: 'h2',
-//   props: {
-//     className: 'greetings',
-//     children: 'Hello World!'
-//   }
-// }; // примерно такую структуру отдаст создание элемента если использовать React
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
