7a5339603fbd81edf87984d868b4e5913c6d9761
ricsi <rihardptrk18@gmail.com>
2025-05-14 09:29:55 +0200

    lesson-5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,21 @@
-import {Component} from 'react';
 import './App.css';
 
-const Header = () => {
-  return <h2>Hello World!</h2>
-}
 
-// const Field = () => { // компонент Field с использованием функции
-//   const holder = 'Enter here';
-//   const styledField = {
-//     width: '300px'
-//   };
-
-//   return <input
-//           placeholder={holder} 
-//           type='text' 
-//           style={styledField}/>
-// }
-
-class Field extends Component { // тот же компонент Field но с использованием класса а не функции
-  render() { // метод render обязателен при использовании класса
-    const holder = 'Enter here';
-    const styledField = {
-      width: '300px'
-    };
-
-    return <input
-            placeholder={holder} 
-            type='text' 
-            style={styledField}/>
-  }
-}
- 
-function Btn() {
-  const text = 'Log in';
-  const logged = true;
-
-  return <button>{logged ? 'Enter' : text}</button> // обычные конструкции if else не работают внутри элементов
-}
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Field/>
-      <Btn/>
+
     </div>
   );
 }
 
-export {Header}; // если захочется использовать отдельный компонент вне файла
 export default App;
+
+
+// React strict mode по функционалу похож на JS use strict
+// можно подключить глобально и в виде деструктуризации
+
+// компонент нужен для обнаружения проблем в приложении
+// активирует доп проверки
+// можно использовать глобально и для отдельных компонентов
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Header } from './App'; // готово к использованию
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <StrictMode>
+    <App/>
+  </StrictMode>
 );
+
+// React strict mode по функционалу похож на JS use strict
+// можно подключить глобально и в виде деструктуризации
+
+// компонент нужен для обнаружения проблем в приложении
+// активирует доп проверки
+// можно использовать глобально и для отдельных компонентов
