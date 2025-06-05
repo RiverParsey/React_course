ac2dcd6721f643732b2c6f69a9015dba77956b18
ricsi <rihardptrk18@gmail.com>
2025-06-05 12:15:31 +0200

    lesson-23

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Button } from './App';
+import BootstrapTest from './BootstrapTest.js';
 import styled from 'styled-components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BigButton = styled(Button)`
   margin: 0 auto;
@@ -16,5 +19,6 @@ root.render(
   <React.StrictMode>
     <App />
     <BigButton as="a">Отправить отчет</BigButton>
+    <BootstrapTest/>
   </React.StrictMode>
 );
