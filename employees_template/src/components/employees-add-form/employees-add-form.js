a989703974e547ea05ab2b2bb78e936699923b32
ricsi <rihardptrk18@gmail.com>
2025-06-05 12:54:38 +0200

    lesson-24

--- employees_template/src/components/employees-add-form/employees-add-form.js
+++ employees_template/src/components/employees-add-form/employees-add-form.js
@@ -3,12 +3,10 @@ import { Component } from 'react';
 import './employees-add-form.css';
 
 class EmployeesAddForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      salary: ''
-    }
+    
+  state = {
+    name: '',
+    salary: ''
   }
 
   onValueChange = (e) => {
@@ -25,6 +23,12 @@ class EmployeesAddForm extends Component {
       salary: ''
     })
   }
+
+  static onLog = () => {
+    console.log('hey')
+  }
+
+  static logged = 'on';
 
   render() {
     const {name, salary} = this.state;
@@ -55,5 +59,8 @@ class EmployeesAddForm extends Component {
     );
   }
 }
+
+EmployeesAddForm.onLog();
+console.log(EmployeesAddForm.logged);
 
 export default EmployeesAddForm;
